b2a32ddea2a77d1e07c5ed6c1a5ec1d9afb59125
joakimjohn <joakim.john.olsson@gmail.com>
2016-05-02 16:55:59 +0200

    Klar med intro och jobbat med komplex
    Introduktionen är i princip klar, kanske någon bild och kod för syns
    skull. Komplexa talen börjar bli bättre.

--- Hemsida/includes/js/script.js
+++ Hemsida/includes/js/script.js
@@ -24,12 +24,61 @@ $(function() {
 		};
 	});
 
+	$('#komplexSvar1').click(function(e) {
+		
+		var correctAnswer = document.getElementById('1b');
+		e.preventDefault();
+
+		if (correctAnswer.checked === true) {
+			showResult('#rightKomplex1')
+		}else {
+			showResult('#wrongKomplex1')
+		};
+	});
+
+	$('#komplexSvar2').click(function(e) {
+		
+		var correctAnswer = document.getElementById('2b');
+		e.preventDefault();
+
+		if (correctAnswer.checked === true) {
+			showResult('#rightKomplex2')
+		}else {
+			showResult('#wrongKomplex2')
+		};
+	});
+
+	$('#komplexSvar3').click(function(e) {
+		
+		var correctAnswer = document.getElementById('3b');
+		e.preventDefault();
+
+		if (correctAnswer.checked === true) {
+			showResult('#rightKomplex3')
+		}else {
+			showResult('#wrongKomplex3')
+		};
+	});
+
+	$('#komplexSvar4').click(function(e) {
+		
+		var correctAnswer = document.getElementById('4c');
+		e.preventDefault();
+
+		if (correctAnswer.checked === true) {
+			showResult('#rightKomplex4')
+		}else {
+			showResult('#wrongKomplex4')
+		};
+	});
+
+
 	$('#LTISvar2').click(function(e) {
 
 		var answer = $('#LTI2').val();
 		e.preventDefault();
 
-		if (answer === "hej") {
+		if (answer === "4") {
 			showResult('#rightLTI2')
 		}else {
 			showResult('#wrongLTI2')
@@ -37,6 +86,6 @@ $(function() {
 	});
 
 	function showResult(a) {
-		$(a).show().delay(200).addClass("in").fadeOut(3500);
+		$(a).show().delay(200).addClass("in").fadeOut(1500);
 	};
 });
