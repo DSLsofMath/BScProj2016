674c81e071ff400696a83df7a1bd782792d4f488
Jacob Jonsson <jassob@live.se>
2016-05-09 10:13:44 +0200

    Uppdaterade kodpaketet till Avsnitt 1.
    Uppdaterade koden till ComplexNumbers.

--- Tutorial/Kod/Kodpaket till studenterna/Avsnitt 1/ComplexNumbers.hs
+++ Tutorial/Kod/Kodpaket till studenterna/Avsnitt 1/ComplexNumbers.hs
@@ -1,95 +1,153 @@
 module ComplexNumbers where
-
 import GHC.Real
 
---  Komplexa tal kan ses som ett par av reella v‰rden.
+-- Komplexa tal kan ses som ett par av reella v√§rden.
 
-data Complex = Complex (Double, Double)
+data Complex = Complex Double Double
     deriving Eq
---  D‰r fˆrsta komponenten representerar realdelen och den andra komponenten imagin‰rdelen.
+
+-- D√§r f√∂rsta komponenten representerar realdelen och den andra komponenten
+-- imagin√§rdelen.
 
 realPart :: Complex -> Double
-realPart (Complex c) = fst c
+realPart (Complex re im) = re
+
+imPart :: Complex -> Double
+imPart = undefined
 
 conjugate :: Complex -> Complex
-conjugate z = Complex (realPart z, negate (imPart z))
+conjugate z = Complex (realPart z) (negate (imPart z))
 
--- | Argumentet av ett komplext tal. Bˆkigt v‰rre pÂ grund av trÂkiga kvadranter och bˆs.
+{-
+   Argumentet av ett komplext tal.
+   B√∂kigt v√§rre p√• grund av tr√•kiga kvadranter och b√∂s,
+   men genom att anv√§nda funktionen atan2 s√• blir det mycket snyggare!
+-}
+
 arg :: Complex -> Double
-arg z   | (imPart z) < 0  && (realPart z) < 0 = atan (imPart z / realPart z) - pi
-        | (realPart z) < 0                  = atan (imPart z / realPart z) + pi
-        | otherwise                         = atan (imPart z / realPart z)
+arg z = atan2 (imPart z) (realPart z)
+
+{-
+   Utan atan2 hade vi f√•tt skriva s√• h√§r:
+
+   arg :: Complex -> Double
+   arg z  | (imPart z) < 0  && (realPart z) < 0 = atan (imPart z / realPart z) - pi
+          | (realPart z) < 0                    = atan (imPart z / realPart z) + pi
+          | otherwise                           = atan (imPart z / realPart z)
+
+-}
 
 scale :: Double -> Complex -> Complex
-scale a z = Complex (a * realPart z, a * imPart z)
+scale a z = Complex (a * realPart z) (a * imPart z)
 
--- j ‰r det komplexa talet med realdelen 0 och imagin‰rdelen 1
--- MÂnga matematiktexter kallar detta talet ocksÂ fˆr `i`
+{-
+   j √§r det komplexa talet med realdelen 0 och imagin√§rdelen 1
+   M√•nga matematiktexter kallar detta talet ocks√• f√∂r `i`
+-}
+
 j :: Complex
-j = Complex (0, 1)
+j = Complex 0 1
 
 instance Show Complex where
     show = printComplex
 
+{-
+   | Printfunktionen f√∂r complexa tal
+   Exempel p√• hur utskriften b√∂r funka:
+   Complex 1 1    <-> 1.0 + 1.0j
+   Complex 0 1    <-> 1.0j
+   Complex 1 0    <-> 1.0
+   Complex 1 (-1) <-> 1.0 - 1.0j
+-}
+printComplex :: Complex -> String
+printComplex = undefined
+
+-- | Skapar ett komplext tal utifr√•n en vinkel.
+euler :: Double -> Complex
+euler phi = Complex (cos phi) (sin phi)
+
+
 instance Num Complex where
+-- Plus √§r f√∂rh√•llandevist trivialt
+  z + w = undefined
+-- Multiplikationen f√∂ljer ifr√•n att vi multiplicerar komponenterna
+-- parvis med varandra (i likhet med (a+b) * (c+d))
+  z * w  = Complex (realZ*realW - imZ*imW) (realZ*imW + realW*imZ)
+    where realZ = realPart z
+          realW = realPart w
+          imZ   = imPart z
+          imW   = imPart w
 
--- Multiplikationen fˆljer ifrÂn att vi multiplicerar komponenterna parvis med varandra (i likhet med (a+b) * (c+d))
-    z * w           = Complex (realZ*realW - imZ*imW, realZ*imW + realW*imZ)
-        where realZ = realPart z
-              realW = realPart w
-              imZ   = imPart z
-              imW   = imPart w
+-- Negationen av ett komplext tal √§r ett komplext tal d√§r b√•da komponenter √§r negerade
 
--- Eftersom komplexa tal inte har nÂgon definition av positiva och negativa tal ‰r signum odefinierad.
-    signum z        = z / abs z
+  negate z = undefined
 
+-- Den naturliga generaliseringen av signum fr√•n reella till komplexa tal:
 
--- Heltal ‰r bara komplexa tal utan imagin‰rdel
-    fromInteger n   = Complex (fromInteger n, 0)
+  signum z = z / abs z
+
+-- Absolutbeloppet av ett komplext tal √§r pythagoras sats p√• dess komponenter.
+
+  abs z = undefined
+
+-- Heltal √§r bara komplexa tal utan imagin√§rdel
+
+  fromInteger n = Complex (fromInteger n) 0
 
 instance Fractional Complex where
--- Division utfˆrs genom att man fˆrl‰nger hela brÂket med n‰mnarens konjugat
-    z / w = Complex (realPart zw' / realPart ww', imPart zw' / realPart ww')
-        where zw' = z * (conjugate w)
-              ww' = w * (conjugate w)
+-- Division utf√∂rs genom att man f√∂rl√§nger hela br√•ket med n√§mnarens konjugat
 
--- En kvot av tvÂ heltal ‰r helt reell och d‰rfˆr kommer imagin‰rdelen vara 0
---TODO: B‰ttre att gˆra som med (fromInteger n) ovan - det blir en effektivare ber‰kning.
-    fromRational z = fromInteger (numerator z) / fromInteger (denominator z)
+  z / w = Complex (realPart zw' / realPart ww') (imPart zw' / realPart ww')
+    where zw' = z * (conjugate w)
+          ww' = w * (conjugate w)
+-- En kvot av tv√• heltal √§r helt reell och d√§rf√∂r kommer imagin√§rdelen vara 0
 
--- Garanterar att imagin‰rdelen alltid ‰r 0 ifrÂn en fromRational
-prop_testRatioNoImpart :: Rational -> Bool
-prop_testRatioNoImpart ratio = imPart (fromRational ratio :: Complex) == 0
+  fromRational z = Complex re 0
+    where re = fromInteger (numerator z) / fromInteger (denominator z)
 
 instance Floating Complex where
--- Det komplexa talet pi ‰r ett tal med realdelen pi och imagin‰rdelen 0
-    pi    = Complex (pi, 0)
 
--- Potenslagarna ger att e^(a+bj) <=> e^a * e^bj och
--- Eulers formel ger att e^bj <=> cos b + jsin b
--- TODO: B‰ttre att gˆra som med (fromInteger n) ovan - det blir en effektivare ber‰kning.
+-- Det komplexa talet pi √§r ett tal med realdelen pi och imagin√§rdelen 0
+  pi    = Complex pi 0
 
-    exp z = scale (exp (realPart z)) (euler (imPart z))
-        -- Skapar ett trigonometriskt komplext tal utifrÂn en vinkel
-        where euler phi = Complex (cos phi, sin phi)
+{-
+   Potenslagarna ger att e^(a+bj) <=> e^a * e^bj och
+   Eulers formel ger att e^bj <=> cos b + jsin b
+-}
+  exp z = scale (exp (realPart z)) (euler (imPart z))
 
--- Eulers formel ger att man kan skriva om sin x som (e^jx - e^-jx)/2j
--- Eftersom vi definerat exponentialfunktionen fˆr komplexa tal kan vi anv‰nda
--- eulers formel som vÂr sinus implementation fˆr komplexa tal
-    sin z = (exp (j*z) - exp (-(j*z)))/(scale 2 j)
+{-
+   Eulers formel ger att man kan skriva om sin x som (e^jx - e^-jx)/2j
+   Eftersom vi definerat exponentialfunktionen f√∂r komplexa tal kan vi anv√§nda
+   eulers formel som v√•r sinus implementation f√∂r komplexa tal
+-}
+  sin z = (exp (j*z) - exp (-(j*z)))/(scale 2 j)
 
 -- Eulers formel ger att man kan skriva om cos x som (e^jx + e^-jx)/2
-    cos z = (exp (j*z) + exp (-(j*z)))/2
+  cos z = (exp (j*z) + exp (-(j*z)))/2
 
-    cosh z = Complex (cosh (realPart z) * cos (imPart z), sinh (realPart z) * sin (imPart z))
-    sinh z = Complex (sin (realPart z) * cosh (imPart z), cos (realPart z) * sinh (imPart z))
+  cosh z = Complex (cosh (realPart z) * cos (imPart z))
+                   (sinh (realPart z) * sin (imPart z))
 
-
--- Eulers formel ger att z = re^(j*phi) och eftersom log och exponentialfunktionen
--- ‰r varandras inverser och logaritmlagarna ger ex. `log (a*b)` <=> `log a + log b`.
--- D‰rfˆr kan vi skriva log `z = log r + log (e^(j*phi))` = `log r + j*phi`
--- TODO: NÂgot fel med ` ovan
-    log z = Complex (logR (abs z), (arg z))
-        where logR = log . realPart
+  sinh z = Complex (sin (realPart z) * cosh (imPart z))
+                   (cos (realPart z) * sinh (imPart z))
 
 
+{-
+   Eulers formel ger att z = re^(j*phi) och eftersom log och exponentialfunktionen
+   √§r varandras inverser och logaritmlagarna ger ex. `log (a*b)` <=> `log a + log b`.
+   D√§rf√∂r kan vi skriva log `z = log r + log (e^(j*phi))` = `log r + j*phi`
+-}
+  log z = Complex (logR (abs z)) (arg z)
+    where logR = log . realPart
+
+{-
+  Funktioner vi troligen / f√∂rhoppningsvis inte kommer beh√∂va och som vi d√§rf√∂r l√§mnar
+  odefinierade √§n s√• l√§nge.
+-}
+  atanh = undefined
+  atan  = undefined
+  asinh = undefined
+  asin  = undefined
+  acosh = undefined
+  acos  = undefined
