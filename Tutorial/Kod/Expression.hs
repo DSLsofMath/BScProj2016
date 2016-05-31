967f17234942d83823556cf1460e6b13539f73fa
Jacob Jonsson <jassob@live.se>
2016-05-31 19:30:38 +0200

    Bytte ut Show instancen till att vara derived.
    Gjorde lite små ändringar i showExp.

--- Tutorial/Kod/Expression.hs
+++ Tutorial/Kod/Expression.hs
@@ -29,7 +29,7 @@ data Expression a =  Const a
                   |  Asinh (Expression a)
                   |  Acosh (Expression a)
                   |  Atanh (Expression a)
-                  deriving (Eq)
+                  deriving (Show,Eq)
 
 -- | Evaluates an Expression to a function
 eval :: (Num a, Floating a, Eq a) => Expression a -> (a -> a)
@@ -88,30 +88,31 @@ instance (Floating a) => Floating (Expression a) where
          asinh = Asinh
          acosh = Acosh
 
--- | Show instance for Showable Expressions
-instance Show a => Show (Expression a) where
-         show = showE
+-- -- | Show instance for Showable Expressions
+-- instance Show a => Show (Expression a) where
+--          show = showE
 
-showE :: (Show a) => Expression a -> String
-showE exp = "f(var) = " ++ showExp exp
+-- showE :: (Show a) => Expression a -> String
+-- showE exp = "f(var) = " ++ showExp exp
 
 showExp :: (Show a) => Expression a -> String
 showExp (Const a)   = show a
-showExp Id          = "var"
+showExp Id          = "id"
 showExp Pi          = "π"
-showExp Impulse     = "δ(var)"
+showExp Impulse     = "δ"
 showExp (Exp e)     = "e^(" ++ showExp e ++ ")"
-showExp (e0 :*: e1) = showCompExps e0 ++ " × " ++ showCompExps e1
+showExp (e0 :*: e1) = showCompExps e0 ++ "" ++ showCompExps e1
 showExp (e0 :+: e1) = showCompExps e0 ++ " + " ++ showCompExps e1
 showExp (e0 :-: e1) = showCompExps e0 ++ " - " ++ showCompExps e1
 showExp (e0 :/: e1) = showCompExps e0 ++ " / " ++ showCompExps e1
+showExp (Conv e0 e1) = showCompExps e0 ++ " * " ++ showCompExps e1
 showExp (Shift offset exp) = showExpWithOffset offset exp
 
 -- | Composed Expressions needs to be enclosed in a pair of parens
 showCompExps :: (Show a) => Expression a -> String
 showCompExps (e0 :+: e1) = "(" ++ showCompExps e0 ++ " + " ++ showCompExps e1 ++ ")"
 showCompExps (e0 :-: e1) = "(" ++ showCompExps e0 ++ " - " ++ showCompExps e1 ++ ")"
-showCompExps (e0 :*: e1) =  showCompExps e0 ++ " × " ++ showCompExps e1
+showCompExps (e0 :*: e1) =  "(" ++ showCompExps e0 ++ ")(" ++ showCompExps e1 ++ ")"
 showCompExps (e0 :/: e1) = "(" ++ showCompExps e0 ++ " / " ++ showCompExps e1 ++ ")"
 showCompExps exp         = showExp exp
 
